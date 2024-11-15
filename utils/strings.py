59d7ad8350bb468e9dc46d6e6f3dbd3b9dacd079
Dreadyyyy <aboutegor8@gmail.com>
2024-11-16 00:52:46 +0300

    Changed start message

--- utils/strings.py
+++ utils/strings.py
@@ -1,14 +1,9 @@
 start = """
 Welcome to telred! The following commands are supported:
-/top
-/hot
-/new
-/controversial
-/rising
-All commands have the following signature:
-```
-command subreddit [time]
-```
+`/top subreddit [time]`
+`/hot subreddit`
+`/new subreddit`
+`/controversial subreddit [time]`
+`/rising subreddit`
 Time is an optional arguments that can be one of the following values: all, day, hour, month, week 
-Note, that optional time argument only has effect for /top and /controversial commands
 """
